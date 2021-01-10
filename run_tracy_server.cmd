93886a44ff74ccc9d5fb9d2874a236580b7cc5ae
Marco Jonkers <contact@marcojonkers.com>
2021-01-10 12:51:03 +0100

    Change Tracy server launch script from Powershell to CMD

---
+++ run_tracy_server.cmd
@@ -0,0 +1,4 @@
+@echo off
+pushd .\3rdparty\tracy\profiler\build\win32\x64\Release\
+start .\Tracy.exe
+popd
--- run_tracy_server.ps1
+++
@@ -1,3 +0,0 @@
-Push-Location .\3rdparty\tracy\profiler\build\win32\x64\Release\
-.\Tracy.exe
-Pop-Location
