b1fa709965fdd002f4e5f00f3c7668987977e0c8
Marco Jonkers <contact@marcojonkers.com>
2021-02-25 23:39:00 +0100

    Add xoshiro128++ random number generator

---
+++ src/mj_random.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "mj_macro.h"
+#include <stdint.h>
+
+namespace mj
+{
+  namespace rng
+  {
+    class xoshiro128plusplus
+    {
+    private:
+      MJ_UNINITIALIZED uint32_t s[4];
+
+    public:
+      void seed(uint32_t s0, uint32_t s1, uint32_t s2, uint32_t s3);
+      uint32_t next();
+      void jump();
+      void long_jump();
+    };
+  } // namespace rng
+} // namespace mj
