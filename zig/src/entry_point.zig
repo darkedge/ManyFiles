0c8c0c3fadbe1dbb24e0ec42d3a272b0ef9d30c2
Marco Jonkers <contact@marcojonkers.com>
2021-03-03 21:51:30 +0100

    Add main_window.zig

--- zig/src/entry_point.zig
+++ zig/src/entry_point.zig
@@ -1,62 +1,6 @@
-const std = @import("std");
-usingnamespace std.os.windows;
-usingnamespace std.os.windows.user32;
-
-const expect = std.testing.expect;
-
-const win32 = @import("win32.zig");
-
-fn WindowProc(hwnd: HWND, message: UINT, wParam: WPARAM, lParam: LPARAM) callconv(.Stdcall) LRESULT {
-    switch (message) {
-        WM_CREATE => std.log.info("All your codebase are belong to us.", .{}),
-        WM_DESTROY => {
-            PostQuitMessage(0);
-            return null;
-        },
-        else => {},
-    }
-
-    return win32.DefWindowProcW(hwnd, message, wParam, lParam);
-}
+const main_window = @import("main_window.zig");
 
 pub fn main() anyerror!void {
-    const hInstance = @intToPtr(HINSTANCE, std.process.getBaseAddress());
-    const className = std.unicode.utf8ToUtf16LeStringLiteral("Class Name");
-    const wc = win32.WNDCLASSEXW{
-        .style = 0,
-        .lpfnWndProc = WindowProc,
-        .cbClsExtra = 0,
-        .cbWndExtra = 0,
-        .hInstance = hInstance,
-        .hIcon = null,
-        .hCursor = null,
-        .hbrBackground = null,
-        .lpszMenuName = null,
-        .lpszClassName = className,
-        .hIconSm = null,
-    };
-    expect(win32.RegisterClassExW(&wc) != 0);
-
-    const hWnd = win32.CreateWindowExW(
-        0,
-        className,
-        std.unicode.utf8ToUtf16LeStringLiteral("Window Title"),
-        win32.WS_OVERLAPPEDWINDOW,
-        win32.CW_USEDEFAULT,
-        win32.CW_USEDEFAULT,
-        win32.CW_USEDEFAULT,
-        win32.CW_USEDEFAULT,
-        null,
-        null,
-        hInstance,
-        null, // TODO: Add application pointer
-    ).?;
-
-    _ = win32.ShowWindow(hWnd, SW_SHOW);
-
-    var msg: MSG = undefined;
-    while (win32.GetMessageW(&msg, null, 0, 0) != 0) {
-        _ = win32.TranslateMessage(&msg);
-        _ = win32.DispatchMessageW(&msg);
-    }
+    const mw = main_window.MainWindow;
+    mw.run();
 }
