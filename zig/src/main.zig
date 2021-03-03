0fa5deabe1c4000caf65be6039d6c344f2eed0d1
Marco Jonkers <contact@marcojonkers.com>
2021-03-03 21:39:14 +0100

    Zig: Move win32 symbols to their own file

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,62 +1,12 @@
 const std = @import("std");
-const expect = std.testing.expect;
 usingnamespace std.os.windows;
 usingnamespace std.os.windows.user32;
 
-pub extern "user32" fn DefWindowProcW(HWND, Msg: UINT, WPARAM, LPARAM) callconv(WINAPI) LRESULT;
-pub extern "user32" fn CreateWindowExW(dwExStyle: DWORD, lpClassName: LPCWSTR, lpWindowName: LPCWSTR, dwStyle: DWORD, X: i32, Y: i32, nWidth: i32, nHeight: i32, hWndParent: ?HWND, hMenu: ?HMENU, hInstance: HINSTANCE, lpParam: ?LPVOID) callconv(WINAPI) ?HWND;
-pub extern "user32" fn GetMessageW(lpMsg: *MSG, hWnd: ?HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT) callconv(WINAPI) BOOL;
-pub extern "user32" fn DispatchMessageW(lpMsg: *const MSG) callconv(WINAPI) LRESULT;
-pub extern "kernel32" fn GetLastError() callconv(WINAPI) DWORD;
-pub extern "kernel32" fn RegisterClassExW(*const WNDCLASSEXW) callconv(WINAPI) ATOM;
+const expect = std.testing.expect;
 
-const WS_POPUP = 0x80000000;
-const WS_CHILD = 0x40000000;
-const WS_MINIMIZE = 0x20000000;
-const WS_VISIBLE = 0x10000000;
-const WS_DISABLED = 0x08000000;
-const WS_CLIPSIBLINGS = 0x04000000;
-const WS_CLIPCHILDREN = 0x02000000;
-const WS_MAXIMIZE = 0x01000000;
-const WS_BORDER = 0x00800000;
-const WS_DLGFRAME = 0x00400000;
-const WS_VSCROLL = 0x00200000;
-const WS_HSCROLL = 0x00100000;
-const WS_GROUP = 0x00020000;
-const WS_TABSTOP = 0x00010000;
+const win32 = @import("win32.zig");
 
-const WS_TILED = WS_OVERLAPPED;
-const WS_ICONIC = WS_MINIMIZE;
-const WS_SIZEBOX = WS_THICKFRAME;
-const WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW;
-
-const WS_OVERLAPPEDWINDOW = (WS_OVERLAPPED |
-    WS_CAPTION |
-    WS_SYSMENU |
-    WS_THICKFRAME |
-    WS_MINIMIZEBOX |
-    WS_MAXIMIZEBOX);
-
-const CW_USEDEFAULT = @truncate(i32, 0x80000000);
-
-const ATOM = WORD;
-
-const WNDCLASSEXW = extern struct {
-    cbSize: UINT = @sizeOf(WNDCLASSEXW),
-    style: UINT,
-    lpfnWndProc: WNDPROC,
-    cbClsExtra: i32,
-    cbWndExtra: i32,
-    hInstance: HINSTANCE,
-    hIcon: ?HICON,
-    hCursor: ?HCURSOR,
-    hbrBackground: ?HBRUSH,
-    lpszMenuName: ?LPCWSTR,
-    lpszClassName: LPCWSTR,
-    hIconSm: ?HICON,
-};
-
-fn WindowProc(hwnd: HWND, message: UINT, wParam: WPARAM, lParam: LPARAM) callconv(WINAPI) LRESULT {
+fn WindowProc(hwnd: HWND, message: UINT, wParam: WPARAM, lParam: LPARAM) callconv(.Stdcall) LRESULT {
     switch (message) {
         WM_CREATE => std.log.info("All your codebase are belong to us.", .{}),
         WM_DESTROY => {
@@ -66,13 +16,13 @@ fn WindowProc(hwnd: HWND, message: UINT, wParam: WPARAM, lParam: LPARAM) callcon
         else => {},
     }
 
-    return DefWindowProcW(hwnd, message, wParam, lParam);
+    return win32.DefWindowProcW(hwnd, message, wParam, lParam);
 }
 
 pub fn main() anyerror!void {
     const hInstance = @intToPtr(HINSTANCE, std.process.getBaseAddress());
     const className = std.unicode.utf8ToUtf16LeStringLiteral("Class Name");
-    const wc = WNDCLASSEXW{
+    const wc = win32.WNDCLASSEXW{
         .style = 0,
         .lpfnWndProc = WindowProc,
         .cbClsExtra = 0,
@@ -85,28 +35,28 @@ pub fn main() anyerror!void {
         .lpszClassName = className,
         .hIconSm = null,
     };
-    expect(RegisterClassExW(&wc) != 0);
+    expect(win32.RegisterClassExW(&wc) != 0);
 
-    const hWnd = CreateWindowExW(
+    const hWnd = win32.CreateWindowExW(
         0,
         className,
         std.unicode.utf8ToUtf16LeStringLiteral("Window Title"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        win32.WS_OVERLAPPEDWINDOW,
+        win32.CW_USEDEFAULT,
+        win32.CW_USEDEFAULT,
+        win32.CW_USEDEFAULT,
+        win32.CW_USEDEFAULT,
         null,
         null,
         hInstance,
         null, // TODO: Add application pointer
     ).?;
 
-    _ = ShowWindow(hWnd, SW_SHOW);
+    _ = win32.ShowWindow(hWnd, SW_SHOW);
 
     var msg: MSG = undefined;
-    while (GetMessageW(&msg, null, 0, 0) != 0) {
-        _ = TranslateMessage(&msg);
-        _ = DispatchMessageW(&msg);
+    while (win32.GetMessageW(&msg, null, 0, 0) != 0) {
+        _ = win32.TranslateMessage(&msg);
+        _ = win32.DispatchMessageW(&msg);
     }
 }
---
+++ zig/src/win32.zig
@@ -0,0 +1,56 @@
+const std = @import("std");
+usingnamespace std.os.windows;
+usingnamespace std.os.windows.user32;
+
+pub const WS_POPUP = 0x80000000;
+pub const WS_CHILD = 0x40000000;
+pub const WS_MINIMIZE = 0x20000000;
+pub const WS_VISIBLE = 0x10000000;
+pub const WS_DISABLED = 0x08000000;
+pub const WS_CLIPSIBLINGS = 0x04000000;
+pub const WS_CLIPCHILDREN = 0x02000000;
+pub const WS_MAXIMIZE = 0x01000000;
+pub const WS_BORDER = 0x00800000;
+pub const WS_DLGFRAME = 0x00400000;
+pub const WS_VSCROLL = 0x00200000;
+pub const WS_HSCROLL = 0x00100000;
+pub const WS_GROUP = 0x00020000;
+pub const WS_TABSTOP = 0x00010000;
+
+pub const WS_TILED = WS_OVERLAPPED;
+pub const WS_ICONIC = WS_MINIMIZE;
+pub const WS_SIZEBOX = WS_THICKFRAME;
+pub const WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW;
+
+pub const WS_OVERLAPPEDWINDOW = (WS_OVERLAPPED |
+    WS_CAPTION |
+    WS_SYSMENU |
+    WS_THICKFRAME |
+    WS_MINIMIZEBOX |
+    WS_MAXIMIZEBOX);
+
+pub const CW_USEDEFAULT = @truncate(i32, 0x80000000);
+
+pub const ATOM = WORD;
+
+pub const WNDCLASSEXW = extern struct {
+    cbSize: UINT = @sizeOf(WNDCLASSEXW),
+    style: UINT,
+    lpfnWndProc: WNDPROC,
+    cbClsExtra: i32,
+    cbWndExtra: i32,
+    hInstance: HINSTANCE,
+    hIcon: ?HICON,
+    hCursor: ?HCURSOR,
+    hbrBackground: ?HBRUSH,
+    lpszMenuName: ?LPCWSTR,
+    lpszClassName: LPCWSTR,
+    hIconSm: ?HICON,
+};
+
+pub extern "user32" fn DefWindowProcW(HWND, Msg: UINT, WPARAM, LPARAM) callconv(.Stdcall) LRESULT;
+pub extern "user32" fn CreateWindowExW(dwExStyle: DWORD, lpClassName: LPCWSTR, lpWindowName: LPCWSTR, dwStyle: DWORD, X: i32, Y: i32, nWidth: i32, nHeight: i32, hWndParent: ?HWND, hMenu: ?HMENU, hInstance: HINSTANCE, lpParam: ?LPVOID) callconv(.Stdcall) ?HWND;
+pub extern "user32" fn GetMessageW(lpMsg: *MSG, hWnd: ?HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT) callconv(.Stdcall) BOOL;
+pub extern "user32" fn DispatchMessageW(lpMsg: *const MSG) callconv(.Stdcall) LRESULT;
+pub extern "kernel32" fn GetLastError() callconv(.Stdcall) DWORD;
+pub extern "kernel32" fn RegisterClassExW(*const WNDCLASSEXW) callconv(.Stdcall) ATOM;
