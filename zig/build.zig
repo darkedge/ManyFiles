208caa1f64775dd5740e7665753c15181da5b320
Marco Jonkers <contact@marcojonkers.com>
2021-03-03 21:43:46 +0100

    Rename main.zig to entry_point.zig

--- zig/build.zig
+++ zig/build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zig-win32", "src/main.zig");
+    const exe = b.addExecutable("zig-win32", "src/entry_point.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
