5b9668b811d20076ad8d380a64c0de589b48da68
Marco Jonkers <contact@marcojonkers.com>
2021-04-10 13:38:27 +0200

    Write a file in CodeGeneration

--- src/CodeGeneration.cpp
+++ src/CodeGeneration.cpp
@@ -1,4 +1,32 @@
 #include <Windows.h>
+#include "ErrorExit.h"
+#include "ServiceProvider.h"
+#include "ServiceLocator.h"
+
+static constexpr const wchar_t* serializationCpp = L"..\\..\\src\\Serialization.cpp";
+
+static void CreateSerializationSourceCode()
+{
+  MJ_UNINITIALIZED HANDLE file;
+  MJ_ERR_IF(
+      file = ::CreateFileW(serializationCpp, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr),
+      INVALID_HANDLE_VALUE);
+  MJ_DEFER(MJ_ERR_ZERO(::CloseHandle(file)));
+
+  mj::ArrayList<wchar_t> al;
+  al.Init(svc::GeneralPurposeAllocator());
+  MJ_DEFER(al.Destroy());
+  mj::StringBuilder sb;
+  sb.SetArrayList(&al);
+
+  sb.Append(L"0");
+
+  mj::StringView sv = sb.ToStringOpen();
+  MJ_UNINITIALIZED DWORD bytesWritten;
+
+  // Write file contents
+  MJ_ERR_ZERO(::WriteFile(file, sv.ptr, sv.len * sizeof(wchar_t), &bytesWritten, nullptr));
+}
 
 /// <summary>
 /// Default entry CRT-less point
@@ -7,16 +35,16 @@ void CALLBACK WinMainCRTStartup()
 {
   HANDLE hStdout          = ::GetStdHandle(STD_OUTPUT_HANDLE);
   const char* lpszPrompt1 = "Hello World!";
-  DWORD cWritten;
-  if (!::WriteFile(hStdout,               // output handle
-                   lpszPrompt1,           // prompt string
-                   lstrlenA(lpszPrompt1), // string length
-                   &cWritten,             // bytes written
-                   NULL))                 // not overlapped
-  {
-    MessageBox(NULL, TEXT("WriteFile"), TEXT("Console Error"), MB_OK);
-    ::ExitProcess(1);
-  }
+  MJ_UNINITIALIZED DWORD cWritten;
+  static_cast<void>(::WriteFile(hStdout, lpszPrompt1, lstrlenA(lpszPrompt1), &cWritten, nullptr));
+
+  mj::HeapAllocator generalPurposeAllocator;
+  generalPurposeAllocator.Init();
+  MJ_UNINITIALIZED mj::AllocatorBase* pAllocator;
+  pAllocator = &generalPurposeAllocator;
+  svc::ProvideGeneralPurposeAllocator(pAllocator);
+
+  ::CreateSerializationSourceCode();
 
   // Running without CRT requires a manual call to ExitProcess
   ::ExitProcess(0);
