61e9546481ba6d6002c4b63dbe6e92d5acf40789
Marco Jonkers <contact@marcojonkers.com>
2021-05-24 17:36:59 +0200

    Revert "Enable unity build"
    This reverts commit d7196f79f492d6fc7e09bcc753b93a812e70607a.

--- src/TabLayout.cpp
+++ src/TabLayout.cpp
@@ -3,6 +3,8 @@
 #include "ResourcesD2D1.h"
 #include "ServiceLocator.h"
 
+static constexpr const int16_t s_ResizeControlWidth = 8;
+
 void mj::TabLayout::Init(AllocatorBase* pAllocator)
 {
   this->pAllocator = pAllocator;
